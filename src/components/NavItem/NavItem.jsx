5b2bda2702814a99387c69dd2945c2f29369b5f2
Janine Holanda <janine.holanda@gmail.com>
2024-12-26 20:04:05 -0500

    Develop navigation item component

---
+++ src/components/NavItem/NavItem.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+
+export default function NavItem({ img, text, textColor }) {
+  const [showText, setShowText] = useState("hidden");
+
+  return (
+    <div
+      className="flex flex-col items-center"
+      onMouseEnter={(e) => {
+        setShowText("block");
+      }}
+      onMouseLeave={(e) => {
+        setShowText("hidden");
+      }}
+    >
+      <div className="mb-1 border-4 rounded-lg border-m-light-blue w-10 h-8 ring-2 ring-black">
+        <div className="ring-2 ring-black rounded-md border-black h-full bg-transparent p-[2px]">
+          <img
+            className="w-full h-full hover:animate-spinY "
+            src={img}
+            alt="image"
+          />
+        </div>
+      </div>
+      <h4
+        className={`${showText} font-mario text-${textColor} text-xs drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)] tracking-wide animate-fadeIn`}
+      >
+        {text}
+      </h4>
+    </div>
+  );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,33 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        mario: ["Mario", "sans-serif"],
+      },
+      colors: {
+        "m-red": "#e71e06",
+        "m-blue": "#019cda",
+        "m-green": "#42b133",
+        "m-yellow": "#fcd000",
+        "m-blue-navy": "#2765d6",
+        "m-light-blue": "#519bde",
+      },
+      keyframes: {
+        spinY: {
+          from: { transform: "rotateY(0deg)" },
+          to: { transform: "rotateY(360deg)" },
+        },
+        fadeIn: {
+          from: { opacity: 0 },
+          to: { opacity: 1 },
+        },
+      },
+      animation: {
+        spinY: "spinY 2s ease-in-out infinite",
+        fadeIn: "fadeIn 1s",
+      },
+    },
   },
   plugins: [],
 };
