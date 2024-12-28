34adbfcf8d2fe3c25a6f9e56c0842c40014fb127
Janine Holanda <janine.holanda@gmail.com>
2024-12-27 19:46:14 -0500

    Add router to main page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import MainPage from "./pages/MainPage/MainPage";
+// import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
+
 export default function App() {
   return (
-    <>
-      <h1 className="text-pink-500 font-bold text-xl">My Journey</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        {/* <Route path="*" element={<NotFoundPage />} /> */}
+      </Routes>
+    </BrowserRouter>
   );
 }
