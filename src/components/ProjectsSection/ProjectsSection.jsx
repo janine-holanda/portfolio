190f69e934bc1790d349d23db18fba334815018b
Janine Holanda <janine.holanda@gmail.com>
2025-01-30 19:22:36 -0500

    Modify project section style

--- src/components/ProjectsSection/ProjectsSection.jsx
+++ src/components/ProjectsSection/ProjectsSection.jsx
@@ -4,9 +4,9 @@ import Card from "../ProjectCard/Card";
 export default function ProjectsSection({ projectsRef }) {
   return (
     <section ref={projectsRef} className="section bg-projects-pattern">
-      <div className="container h-full">
+      <div className="container h-full flex flex-col items-center">
         <h1 className="text-m-yellow">Projects</h1>
-        <div className="flex flex-wrap justify-center gap-6">
+        <div className="flex flex-wrap justify-center gap-6 max-w-screen-lg">
           {projectsData.map((project) => {
             return <Card key={project.id} project={project} />;
           })}
