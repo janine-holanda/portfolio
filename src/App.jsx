11068daf1e1754f8d6da146f105e715533f9e598
Janine Holanda <janine.holanda@gmail.com>
2025-03-11 16:56:04 -0400

    Switch from BrowserRouter to HashRouter and update base path in Vite configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import MainPage from "./pages/MainPage/MainPage";
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<MainPage />} />
         {/* <Route path="*" element={<NotFoundPage />} /> */}
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
