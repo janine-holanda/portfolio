9526787a05b194473053a51a62b66b7d4764b896
Janine Holanda <janine.holanda@gmail.com>
2025-03-12 11:07:48 -0400

    Update layout and styling

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -11,7 +11,7 @@ export default function NavBar({
 }) {
   return (
     <header className="fixed w-full pb-2 pt-4 z-50 backdrop-blur-lg bg-white/5 shadow-md ring-1 ring-black/5 h-fit  sm:h-full sm:w-16 sm:py-0 ">
-      <nav className="container sm:h-full">
+      <nav className="container w-full px-2 sm:w-4/5 sm:px-0 sm:h-full">
         <ul className="grid grid-cols-5 h-full items-center sm:grid-cols-1 sm:grid-rows-5">
           <li onClick={() => handleScrollToSection(homeRef)}>
             <NavItem
