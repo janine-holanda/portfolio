ce706668dcbdfbb86bec685c7813b36ac7a8f978
Janine Holanda <janine.holanda@gmail.com>
2025-03-12 14:26:35 -0400

    Refactor SkillsSection layout and improve technology item hover effects

--- src/components/SkillsSection/SkillsSection.jsx
+++ src/components/SkillsSection/SkillsSection.jsx
@@ -3,43 +3,31 @@ import skills from "../../data/skills.json";
 export default function SkillsSection({ skillsRef }) {
   return (
     <section ref={skillsRef} className="section bg-skills-pattern">
-      <div className="container h-full">
+      <div className="container min-h-screen">
         <h1 className="text-m-red">Skills</h1>
-        <div className="grid auto-rows-min justify-items-center gap-10 sm:grid-cols-2 lg:grid-cols-3 lg:gap-y-28">
+        <div className="min-h-screen grid auto-rows-min justify-items-center content-between justify-evenly gap-8 sm:grid-cols-2 sm:h-[85%] overflow-hidden">
           {skills.map((item) => {
-            if (item.category !== "Next Adventures") {
-              return (
-                <div key={item.id}>
-                  <h2 className=" text-m-red">{item.category}</h2>
-                  <div className="flex justify-center flex-wrap gap-4 sm:flex-col">
-                    {item.technologies.map((skill) => (
+            return (
+              <div key={item.id}>
+                <h2 className=" text-m-red">{item.category}</h2>
+                <div className="py-4 flex justify-center flex-wrap gap-4 sm:flex-col sm:items-center">
+                  {item.technologies.map((skill) => (
+                    <div
+                      key={skill.id}
+                      className="relative w-fit group cursor-pointer hover:motion-scale-in-110 hover:motion-paused"
+                    >
+                      <div className="absolute -inset-[0.15rem] bg-gradient-to-r from-red-600 to-m-red rounded-md blur opacity-0 group-hover:opacity-100 transition duration-1000 group-hover:duration-200"></div>
+
                       <img
-                        key={skill.id}
-                        className="object-contain max-w-full h-6 opacity-90 hover:motion-scale-in-110 hover:motion-paused hover:motion-opacity-in-100"
+                        className="relative object-contain max-w-full h-6 space-x-6"
                         src={skill.src}
                         alt={skill.name}
                       />
-                    ))}
-                  </div>
+                    </div>
+                  ))}
                 </div>
-              );
-            } else {
-              return (
-                <div key={item.id} className="lg:col-span-3 lg:self-top">
-                  <h2 className=" text-m-red">{item.category}</h2>
-                  <div className="flex justify-center flex-wrap gap-4 sm:flex-col lg:flex-row">
-                    {item.technologies.map((skill) => (
-                      <img
-                        key={skill.id}
-                        className="object-contain max-w-full h-6 opacity-90 hover:motion-scale-in-110 hover:motion-paused hover:motion-opacity-in-100"
-                        src={skill.src}
-                        alt={skill.name}
-                      />
-                    ))}
-                  </div>
-                </div>
-              );
-            }
+              </div>
+            );
           })}
         </div>
       </div>
