06f69274e6c061ac269333e110f9ba04b0249bc3
Janine Holanda <janine.holanda@gmail.com>
2025-03-12 14:26:59 -0400

    Enhance ProjectCard hover effects with transition and gradient changes

--- src/components/ProjectCard/Card.jsx
+++ src/components/ProjectCard/Card.jsx
@@ -34,9 +34,9 @@ export default function Card({ project }) {
   return (
     <article
       key={project.id}
-      className={`h-[22rem] w-[18rem] rounded-lg p-[3px] shadow-2xl relative ${
+      className={`h-[22rem] w-[18rem] rounded-lg p-[3px] shadow-2xl relative transition-all duration-1000 ${
         showDetails
-          ? "bg-[linear-gradient(60deg,_rgb(247,_149,_51),_rgb(243,_112,_85),_rgb(239,_78,_123),_rgb(161,_102,_171),_rgb(80,_115,_184),_rgb(16,_152,_173),_rgb(7,_179,_155),_rgb(111,_186,_130))]"
+          ? "bg-[linear-gradient(60deg,_rgb(247,_149,_51),_rgb(243,_112,_85),_rgb(239,_78,_123),_rgb(161,_102,_171),_rgb(80,_115,_184),_rgb(16,_152,_173),_rgb(7,_179,_155),_rgb(111,_186,_130))] hover:bg-[linear-gradient(60deg,_rgb(111,_186,_130),_rgb(7,_179,_155),_rgb(16,_152,_173),_rgb(80,_115,_184),_rgb(161,_102,_171),_rgb(239,_78,_123),_rgb(243,_112,_85),_rgb(247,_149,_51))]"
           : "hover:bg-[linear-gradient(60deg,_rgb(247,_149,_51),_rgb(243,_112,_85),_rgb(239,_78,_123),_rgb(161,_102,_171),_rgb(80,_115,_184),_rgb(16,_152,_173),_rgb(7,_179,_155),_rgb(111,_186,_130))]"
       } `}
     >
